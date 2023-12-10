0f3913adf959c7d49a2b1eaaedfb5fa9af00768d
adithyashibu <adithyas447@gmail.com>
2023-12-10 20:45:23 +0530

    python

---
+++ mymodule.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt 
+# assigning x and y coordinates 
+y = [0,1,2,3,4,5]
+x= [0,5,10,15,20,25]
+# depicting the visualization
+plt.plot(x, y, color='green') 
+plt.xlabel('x') 
+plt.ylabel('y') 
+# displaying the title
+plt.title("Linear graph")
+plt.show()
